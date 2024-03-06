b1281d43b32a42f621006ee898ac8599bc1954d4
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-03-06 12:51:15 -0500

    Poo 2

--- src.js
+++ src.js
@@ -26,5 +26,61 @@ const Perro = {
         odiaALosGatos: true
     }
 }
+const Perro2 = {
+    nombre: "Paco",
+    edad: 4,
+    raza: "Rata",
+    color: "Blanco",
+    nombrarPerro(nombre){
+        return this.nombre = nombre;
+    },
+    caracteristicas:{
+        gay: false,
+        territorial: true,
+        celoso: false,
+        altura: "58cm",
+        comida: "Nutrecan",
+        odiaALosGatos: true
+    }
+}
+//-----------
 
+const Vec = [Perro, Perro2];
+Vec.forEach(animal => {
+    //Cambiar propiedad nombre a cada objeto de la lista
+    animal.nombre = "Cansón";
+});
+
+console.log(Perro);
+console.log(Perro.caracteristicas.comida);
+
+//Segunda forma - No indicada
+const Auto = {};
+Auto.model = "1986";
+Auto.color = "Verde";
+Auto.caracteristicas = {
+    marca:"CosteñoxCars",
+    kms: 72837,
+}
+
+console.log(Auto.caracteristicas);
+
+//Tercera forma
+let marca = "FuckMercedez";
+let model = "JavaRun";
+let caracteristicas = {
+
+}
+
+//Operador de propagacion
+const str = "Nelson in desarrollo web";
+const charArray = [...str];
+console.log(charArray);
+
+const Perro3 = {...Perro, ...Perro2}
+console.log(Perro3)
+
+const numbers = [10, 20, 30];
+const sum = (a, b, c) => a + b + c;
+console.log(sum(...numbers)); 
 
